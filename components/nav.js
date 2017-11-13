ed4dbbfcd62aea0bfe32a844edc1232b6ff45cb9
Thierry Charbonnel <thierryc@users.noreply.github.com>
2017-11-12 20:31:16 -0500

    update link

--- components/nav.js
+++ components/nav.js
@@ -3,6 +3,6 @@ import Link from 'next/link'
 
 
 export default () => <nav>
-  <span><Link href={'/index'}><a>Home</a></Link></span>
-  <span><Link href={'/ap-grid-layout'}><a>Grid Layout</a></Link></span>
+  <span><Link href={'./index'}><a>Home</a></Link></span>
+  <span><Link href={'./ap-grid-layout'}><a>Grid Layout</a></Link></span>
 </nav>
