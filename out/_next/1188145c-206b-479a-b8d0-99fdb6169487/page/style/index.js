52365dac3278c2681eb48b948ea2cbe5a718214a
Thierry Charbonnel <thierryc@users.noreply.github.com>
2017-11-14 16:31:27 -0500

    Deploy gh-pages

---
+++ out/_next/1188145c-206b-479a-b8d0-99fdb6169487/page/style/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/style', function() {
+            var comp = module.exports=webpackJsonp([4],{415:function(o,n,s){o.exports=s(416)},416:function(o,n,s){"use strict";function e(o){return r.colors[o]}Object.defineProperty(n,"__esModule",{value:!0});var r=s(79),c=["body,html{font-family:'Encode Sans',sans-serif;background-color:"+e("backgroundColor")+";}"];c.__hash="1580237661",c.__scoped=["body.jsx-349677532,html.jsx-349677532{font-family:'Encode Sans',sans-serif;background-color:"+e("backgroundColor")+";}"],c.__scopedHash="349677532",n.default=c}},[415]);
+            return { page: comp.default }
+          })
+        
